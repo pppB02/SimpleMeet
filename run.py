e41876c9b1d0f0d0142d05a7edaee1d7dcedc55a
pppB02 <pappbalazs02@gmail.com>
2026-03-01 11:43:26 +0100

    proper routing added

--- run.py
+++ run.py
@@ -1,35 +1,8 @@
-from flask import Flask, render_template, url_for, request, redirect
-from app.init import create_app, db
-import app.db_models as models
-from datetime import datetime, time, timedelta
+from app import create_app
 
 app = create_app()
 
-@app.route("/", methods=['POST','GET'])
-def index():
-
-    tb_model = models.UserAccount.query.all()
-    print(tb_model)
-
-    return render_template("test.html", tb_model=tb_model)
-
 if __name__ == "__main__":
-    with app.app_context():
-
-        # Ha tiszta adatbázist akarsz:
-        db.drop_all()
-        db.create_all()
-
-        admin_user = models.UserAccount(
-            email="admin@test.hu",
-            name="Admin József",
-            password_hash="hashed_pw",
-            role="business_admin"
-        )
-
-        db.session.add(admin_user)
-        db.session.commit()
-
     app.run(debug=True)
 
 
