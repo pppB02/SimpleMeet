8a994cc354174aa6bfb75330f45c6e95d5e86fff
pppB02 <pappbalazs02@gmail.com>
2026-03-21 12:21:03 +0100

    business login and signup pages

--- app/__init__.py
+++ app/__init__.py
@@ -13,14 +13,14 @@ def create_app():
     app.secret_key = os.getenv("APP_KEY")
 
     from app.user.routes_user import user
-    from app.admin.routes_admin import admin
+    from app.business.routes_business import business
     from app.index.routes_index import index
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET KEY'] = os.getenv("DB_PASSWORD")
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['SESSION_PERMANENT']= False
+    #app.config['SESSION_PERMANENT']= "simplemeet.webhop.me"
 
     login_manager.init_app(app)
     login_manager.login_view = "user.login"
@@ -28,7 +28,7 @@ def create_app():
     db.init_app(app)
 
     app.register_blueprint(user)
-    app.register_blueprint(admin)
+    app.register_blueprint(business)
     app.register_blueprint(index)
 
     return app
---
+++ app/business/routes_business.py
@@ -0,0 +1,71 @@
+from flask import Blueprint, render_template, url_for, request, redirect, flash
+from flask_login import logout_user, current_user, login_required
+from ..user.services.customer.forms import SingUpForm, LoginForm
+from app import db, login_manager
+from ..user.services.customer.sign_up import signUpSrv
+from ..user.services.customer.login import loginSrv
+
+business = Blueprint("business", __name__, static_folder="static", template_folder="templates", url_prefix="/business")
+
+@business.route("/")
+def index():
+    return redirect(url_for("business.login"))
+
+@business.route("/login", methods=['POST','GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('business.dashboard'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        if request.method == "POST":
+            email = form.email.data
+            password = form.password.data
+            remember = form.remember.data
+
+            try:
+                loginSrv(email,password,remember)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('business.dashboard'))
+            except Exception as e:
+                flash("bad password or email","danger")
+                return render_template("login/login.html", form=form)
+    else:
+        return render_template("login/login.html", form=form)
+
+@business.route("/logout", methods=['POST','GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+@login_manager.user_loader
+def load_user(user_id):
+    from ..db_models import UserAccount
+    return UserAccount.query.get(int(user_id))
+
+@business.route("/sign-up", methods=['POST','GET'])
+def signUp():
+    if current_user.is_authenticated:
+        return redirect(url_for('business.dashboard'))
+    
+    form = SingUpForm()
+    if form.validate_on_submit():
+        if request.method == "POST":
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+            try:
+                signUpSrv(db,username,email,password,"business_admin")
+                return redirect(url_for("business.dashboard"))
+            except Exception as e:
+                print(e)
+                return "hiba"
+    else:
+        return render_template("sign_up/sign_up.html", form=form)
+    
+@business.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard/dashboard.html")
