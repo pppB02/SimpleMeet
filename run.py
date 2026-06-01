4d548ac6e32e0024b01a22772e6ac00f8e3fedd9
pppB02 <pappbalazs02@gmail.com>
2026-06-01 21:03:52 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
 from app import create_app, db
-from app.db_models import Business
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
-        # db.drop_all()
-        # db.create_all()
-        businesses = Business.query.all()
-
-        for business in businesses:
-            print(business.public_id)
+        db.drop_all()
+        db.create_all()
             
     app.run(debug=True, host="0.0.0.0")
